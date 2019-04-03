0a875410ba35b2c8a9a1cb4d924f24d050fec47b
werefox07 <oxana07zaharova@gmail.com>
2019-04-03 22:35:36 +0300

    добавлены 2 датчика, проверка на их наличие, пауза и возобновление

--- app/src/main/java/ru/zaharova/oxana/gym/fragments/SensorsViewFragment.java
+++ app/src/main/java/ru/zaharova/oxana/gym/fragments/SensorsViewFragment.java
@@ -10,27 +10,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
-import java.util.Objects;
-
 import ru.zaharova.oxana.gym.R;
-
-import static android.support.v4.content.ContextCompat.getSystemService;
 
 @SuppressLint("ValidFragment")
 public class SensorsViewFragment extends Fragment {
-    private TextView textConsole;
-    private TextView textLight;
-    private SensorManager sensorManager;
-    private List<Sensor> sensors;
-    private Sensor sensorLight;
+    private TextView textHum;
+    private TextView textTemp;
     private Context context;
+    private SensorManager sensorManager;
+    private Sensor sensorTemp;
+    private Sensor sensorHum;
 
     public SensorsViewFragment(Context context) {
         super();
@@ -41,61 +35,69 @@ public class SensorsViewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_sensors_view, container, false);
-        textConsole = root.findViewById(R.id.textConsole);
-        textLight = root.findViewById(R.id.textLight);
-        // Менеджер датчиков
+        textTemp = root.findViewById(R.id.textTemp);
+        textHum = root.findViewById(R.id.textHum);
+
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-        // Получить все датчики, какие есть
-        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        // Датчик освещенности (он есть на многих моделях)
-        sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        // Регистрируем слушатель датчика освещенности
-        sensorManager.registerListener(listenerLight, sensorLight, SensorManager.SENSOR_DELAY_NORMAL);
-        // Показать все сенсоры, какие есть
-        showSensors();
+
+        sensorTemp = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if(sensorTemp != null) {
+            sensorManager.registerListener(listenerTemp, sensorTemp, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            textTemp.setText(getString(R.string.sensor_temp_error));
+        }
+
+        sensorHum = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        if(sensorHum != null) {
+            sensorManager.registerListener(listenerHum, sensorHum, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            textHum.setText(getString(R.string.sensor_hum_error));
+        }
         return root;
     }
 
-    // Вывод всех сенсоров
-    private void showSensors() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Sensor sensor : sensors) {
-            stringBuilder.append("name = ").append(sensor.getName())
-                    .append(", type = ").append(sensor.getType())
-                    .append("\n")
-                    .append("vendor = ").append(sensor.getVendor())
-                    .append(" ,version = ").append(sensor.getVersion())
-                    .append("\n")
-                    .append("max = ").append(sensor.getMaximumRange())
-                    .append(", resolution = ").append(sensor.getResolution())
-                    .append("\n").append("--------------------------------------").append("\n");
-        }
-        textConsole.setText(stringBuilder);
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listenerTemp, sensorTemp);
+        sensorManager.unregisterListener(listenerHum, sensorHum);
     }
 
-    // Вывод датчика освещенности
-    private void showLightSensors(SensorEvent event){
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(listenerTemp, sensorTemp, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listenerHum, sensorHum, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void showSensorValue(SensorEvent event, String title, TextView textView){
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Light Sensor value = ").append(event.values[0])
+        stringBuilder.append(title).append(event.values[0])
                 .append("\n").append("=======================================").append("\n");
-        textLight.setText(stringBuilder);
+        textView.setText(stringBuilder);
     }
 
-    // Слушатель датчика освещенности
-    SensorEventListener listenerLight = new SensorEventListener() {
-
+    SensorEventListener listenerTemp = new SensorEventListener() {
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
 
         @Override
         public void onSensorChanged(SensorEvent event) {
-            showLightSensors(event);
+            showSensorValue(event, "Temperature Sensor value = ", textTemp);
         }
     };
 
+    SensorEventListener listenerHum = new SensorEventListener() {
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
 
-
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            showSensorValue(event, "Humidity Sensor value = ", textHum);
+        }
+    };
 }
 
 
